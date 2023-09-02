815d35a6b9e771d3989cd13e457f3f9f44f4564e
Aqsa Ashfaq <aqsarao077@gmail.com>
2023-09-03 01:25:15 +0500

    ex_11

--- exercise_11.ts
+++ exercise_11.ts
@@ -1,6 +1,8 @@
 // Names: Store the names of a few of your friends in a array called names. Print each person’s name 
 // by accessing each element in the list, one at a time.
 
+export {friends_names};
+
 let friends_names:string[] = ["John", "Jane", "Mary", "Peter", "Katie"];
 for (let friend_name of friends_names){
     console.log(friend_name);
