62662b8090bc4471f68fd51a17c5abe6a189d0f7
Aqsa Ashfaq <aqsarao077@gmail.com>
2023-09-03 00:58:13 +0500

    ex_5

--- exercise_5.ts
+++ exercise_5.ts
@@ -5,4 +5,4 @@
 let famous_quote:string = "Do not feel lonely, the entire universe is inside you.";
 let famous_person:string = "Molana Rumi";
 let message:string = `${famous_person} once said: \"${famous_quote}\".`
-console.log(message)
+console.log(message) 
