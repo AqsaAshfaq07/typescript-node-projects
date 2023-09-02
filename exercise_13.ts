a232158f8d67de5441e183d84344895cd62358b9
Aqsa Ashfaq <aqsarao077@gmail.com>
2023-09-03 01:38:32 +0500

    ex_13

---
+++ exercise_13.ts
@@ -0,0 +1,8 @@
+// Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and 
+// make a list that stores several examples. Use your list to print a series of statements about these 
+// items, such as “I would like to own a Honda motorcycle.”
+
+let transportation_modes:string[] = ['bicycle', 'heavy bike', 'car', 'train', 'ship', 'aeroplane'];
+for (let vehicle of transportation_modes){
+    console.log(`My favorite mode of transportation is ${vehicle}`);
+};
