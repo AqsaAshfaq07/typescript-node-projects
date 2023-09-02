8d1ac69e40a13268e6aa97e78126269ed821a67a
Aqsa Ashfaq <aqsarao077@gmail.com>
2023-09-03 01:14:39 +0500

    ex_10

---
+++ exercise_10.ts
@@ -0,0 +1,10 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+// If you don’t have anything specific to write because your programs are too simple at this point, 
+// just add your name and the current date at the top of each program file. Then write one sentence 
+// describing what the program does.
+
+let my_name:string = "AQSA";
+let now = new Date();
+console.log(`Het this is ${my_name} and currently its ${now}.`);
+
+// This program is about to print the name and current date. ps. comments :)
