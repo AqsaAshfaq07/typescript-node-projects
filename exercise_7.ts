7f3ee44542fa82c35f5c9a8a71f564d88e195e7b
Aqsa Ashfaq <aqsarao077@gmail.com>
2023-09-03 01:03:42 +0500

    ex_7

---
+++ exercise_7.ts
@@ -0,0 +1,10 @@
+// Number Eight: Write addition, subtraction, multiplication, and division operations that each 
+// result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+let num1:number = 4;
+let num2:number = 4;
+
+console.log(`Addition: ${num1 + num2}`);
+console.log(`Subtraction: ${16 - num1 - num2}`);
+console.log(`Multiplication : ${(num1 * num2) / 2}`);
+console.log(`Division: ${(num1 / num2) * 8}`);
