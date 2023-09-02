cb191eaa30c685dc2f47667c60eb1a9b1d337de9
Aqsa Ashfaq <aqsarao077@gmail.com>
2023-09-03 01:10:55 +0500

    ex_9

---
+++ exercise_9.ts
@@ -0,0 +1,6 @@
+// Favorite Number: Store your favorite number in a variable. Then, using that variable, create a 
+// message that reveals your favorite number. Print that message.
+
+let favorite_number:number = 7;
+let message:string = `Hey!! ${favorite_number} is my favorite number. :))`;
+console.log(message);
