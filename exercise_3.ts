b44084f46a0030f21f0b167f9e9484dcde5a8711
Aqsa Ashfaq <aqsarao077@gmail.com>
2023-09-03 00:37:56 +0500

    ex_3

---
+++ exercise_3.ts
@@ -0,0 +1,8 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s 
+// name in lowercase, uppercase, and titlecase.
+
+let person_name:string = "Aqsa";
+
+console.log(`Your name in UpperCase is: ${person_name.toUpperCase()}`);
+console.log(`Your name in LowerCase is: ${person_name.toLowerCase()}`);
+console.log(`Your name in TitleCase is: ${person_name[0].toUpperCase()}${person_name.slice(1, )}`)
